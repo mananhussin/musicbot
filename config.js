0c58f9ee8f360761308172960486606c08c8cb2b
ItzLightyHD <itzlightyhd@gmail.com>
2020-07-27 21:19:18 +0200

    Oops

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     "token": process.env.TOKEN,
-    "prefix": process.env.PREFIX0,
+    "prefix": process.env.PREFIX,
     "owners": [process.env.OWNERID],
     "host": process.env.HOST,
     "hostpassw": process.env.HOSTPASSW
