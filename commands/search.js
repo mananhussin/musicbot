ee06423ba543e77d8c8dc357c0603169c7ca7ba4
ItzLightyHD <nethernetwork@libero.it>
2020-08-31 20:07:48 +0200

    New search...

--- commands/search.js
+++ commands/search.js
@@ -1,16 +1,20 @@
 const search = require("yt-search");
+const Discord = require("discord.js");
 
 exports.run = (client, message, args, ops) => {
   search(args.join(' '), function(err, res) {
     if (err) message.reply('Sorry, something went wrong.');
     
     let videos = res.videos.slice(0, 10);
-    let resp = '';
-    for(var i in videos) {
-      resp += `**[${parseInt(i)+1}]:** \`${videos[i].title}\`\n`;
-    }
-    resp += `\n**Choose a number between \`1-${videos.length}\`**`;
-    message.channel.send(resp);
+    const musicEmbed = new Discord.MessageEmbed()
+    .setAuthor("Song selection", message.author.displayAvatarUrl)
+    .setDescription(for(var i in videos) {
+      let resp = `**[${parseInt(i)+1}]:** \`${videos[i].title}\`\n`;
+      return resp;
+    })
+    .setFooter("Choose a number between" + videos.length)
+    .setColor("RANDOM");
+    message.channel.send(musicEmbed);
     
     const filter = m => !isNaN(m.content) && m.content < videos.length+1 && m.content > 0;
     const collector = message.channel.createMessageCollector(filter);
