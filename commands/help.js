db6f1579dc0441d640e96ab9fb69ef1b193e563a
ItzLightyHD <nethernetwork@libero.it>
2020-08-31 19:27:53 +0200

    Some maintenance updates to make all easier

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args, ops) => {
   helpMsg = new Discord.MessageEmbed()
-  .setTitle(process.env.BOT_NAME)
-  .setDescription("Thank you for using **" + process.env.BOT_NAME + "**, here are all the commands.\nIf you have an issue with a command, report it to us using `report bug`")
+  .setTitle(client.user.username)
+  .setDescription("Thank you for using **" + client.user.username + "**, here are all the commands.\nIf you have an issue with a command, report it to us using `report bug`")
   .addField("🔊 Music", "`play`, `leave`, `pause`, `resume`, `search`, `skip`, `musiclink`, `volume`")
   .addField("👮‍ Moderation", "`clear` (or `purge`), `ban`, `kick`, `nick`, `mute`, `unmute`, `whois`, `destroy`")
   .addField("🔥 Fun and Misc", "`help`, `invite`, `mcstatus`, `mcskin` (or `skin`), `8ball`, `meme`, `fotd`, `mcleaks`, `ping`")
